927d00eb854b5f13909e848a819b3ba0f12e065e
Leon22133 <isip_l.g.andreev@mpt.ru>
2023-02-08 16:34:00 +0300

    Текстовый конвертер

---
+++ ConsoleApp35/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace converterLeo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cursor cursor = new Cursor();
+            cursor.offset = 3;
+            Console.WriteLine("Путь до файла");
+            OpenedFile openedFile = new OpenedFile();
+            openedFile.path = Console.ReadLine();
+            Console.Clear();
+            openedFile.text = Editor.Edit(openedFile.path);
+
+            Converters.Convert(openedFile.path);
+        }
+    }
+}
